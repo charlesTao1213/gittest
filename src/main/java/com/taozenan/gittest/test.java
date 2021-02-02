5631913fe3abfd8f98eb650dd2cebcc5d883c8e9
taozenan <taozenan@yuanshenginfo.com>
2021-02-02 18:17:25 +0800

    master

--- src/main/java/com/taozenan/gittest/test.java
+++ src/main/java/com/taozenan/gittest/test.java
@@ -15,7 +15,7 @@ package com.taozenan.gittest;
 public class test {
 
     public  void  testMethod(){
-        System.out.println("test1ddd");
+        System.out.println("master");
         System.out.println("test1");
     }
 }
